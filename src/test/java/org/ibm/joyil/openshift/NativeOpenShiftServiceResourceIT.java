766a3e618db63b85980d431958d957af7ef5c07e
Joyil Joseph <joyiljoseph@gmail.com>
2021-05-04 13:54:11 -0400

    commit to master

---
+++ src/test/java/org/ibm/joyil/openshift/NativeOpenShiftServiceResourceIT.java
@@ -0,0 +1,9 @@
+package org.ibm.joyil.openshift;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeOpenShiftServiceResourceIT extends OpenShiftServiceResourceTest {
+
+    // Execute the same tests but in native mode.
+}
